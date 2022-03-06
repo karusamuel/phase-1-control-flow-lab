db32e5d516100e64226580acef7b1333fcddb129
karu samuel ngugi <karusamuelngugi@gmail.com>
2022-03-06 11:45:15 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distant){
   // Write your code here!
+  if (distant<=400){
+    return "This one is on me!";
+  }
+  else if(distant>2500){
+      return "No can do.";
+    
+  }else if(distant>2000){
+    return "I will gladly take your thirty bucks.";
+  }
+
+
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city ==="NYC" ? "Ok, sounds good.":"No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+  if(tip === 'generous'){
+    return 'Thank you so much.';
+  }else if(tip === 'not as generous'){
+    return 'Thank you.';
+  }else{
+    return 'Bye.';
+  }
 }
